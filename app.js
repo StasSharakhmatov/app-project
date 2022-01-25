937ebf743813d241804a1c6b7814df5872cf2367
StasSharakhmatov <86169378+StasSharakhmatov@users.noreply.github.com>
2022-01-25 21:12:04 +0200

    Delete comments

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-// Подключаем модули
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-// 1. Сервер
-http.createServer( (req, res) => {
-    // Внутри сервера необходимо задать роутинг...
-    // В данном случае будет работать только с тремя видами адресов: 1) Корень - стартовую страницу (когда обращаются к имени по серверу);
-    // 2) Если обращаются к загрузке файлов (в данном случае загрузка файлов идет в папку uploads, необходимо как-то на это реагировать).
-    // 3) Работа со статикой - если загружаются css, html, js файлы, картинки. Необходимо обработать статику и отдать её. 
+http.createServer( (req, res) => { 
     console.log(`req: ${req.url}`);
     if (req.url === '/') {
-        sendRes('index.html', 'text/html', res); // функции передается res, чтобы она смогла ответить за нас 
+        sendRes('index.html', 'text/html', res); 
     } else if (/\/uploads\/[^\/]+$/.test(req.url) && req.method === 'POST') {
-    // необходимо вызвать обработчик файлов
         console.log('upload files');
         saveUploadFile(req, res);
     } else {
@@ -23,14 +16,12 @@ http.createServer( (req, res) => {
     console.log('server start 3000');
 });
 
-// 2. Отправка ресурсов (обработка ресурсов)
 function sendRes(url, contentType, res) {
-    // Переменная file сформирует путь
-    let file = path.join(__dirname + '/static/', url); // __dirname - спец параметр, который указывает имя папки на сервере в которой всё это происходит
+    let file = path.join(__dirname + '/static/', url);
     fs.readFile(file, (err, content) => {
         if (err) {
-            res.writeHead(404); // устанавливает заголовок
-            res.write('file not found'); // сообщаем ответ
+            res.writeHead(404);
+            res.write('file not found');
             res.end();
             console.log(`error 404 ${file}`);
         } else {
@@ -41,8 +32,6 @@ function sendRes(url, contentType, res) {
         }
     })
 }
-
-// 3. Тип контента (для отправки ресурсов необходимо получить тип контента внутри них)
 
 function getContentType(url) {
     switch (path.extname(url)) {
@@ -59,33 +48,22 @@ function getContentType(url) {
     }
 }
 
-// 4. Сохранение файла (из двух частей). Вторая - AJAX запрос, который отправляет всё на сервер.
-// Первая - обработчик сервера.
-
 function saveUploadFile(req, res) {
-// Во-первых, необходимо получить имя файла
     let fileName = path.basename(req.url);
-// Также, необходимо получить путь к файлу
     let file = path.join(__dirname, 'uploads', fileName);
-// Теперь мы должны получить имя папки, куда будем выгружать (в данном случае папка images)
     let imageFolder = path.join(__dirname, 'static/images', fileName);
 
-// Чтение файлов. createWriteStream - читаем поток
     req.pipe(fs.createWriteStream(file));
-// Дальше вешаем событие, когда чтение файла закончиться
     req.on('end', () => {
-// Если всё пошло хорошо, то будем обрабатывать копирование файла
-        fs.copyFile(file, imageFolder, err => { // копируюется из file в imageFolder
+        fs.copyFile(file, imageFolder, err => {
             if (err) {
                 console.log(err);
             } else {
-// После копирования файла, копия файла остается в папке uploads, поэтому необходимо удалить файл
                 fs.unlink(file, err => {
                     console.log(err);
                 })
             }
         })
-// После копирование необходим отдать клиенту информацию о файле: записать заголовки и передать content type
         res.writeHead(200, {'Content-Type': 'text'});
         res.write(fileName);
         res.end();
--- static/js/upload.js
+++ static/js/upload.js
@@ -1,13 +1,13 @@
 function uploadFile(event) {
     console.log('upload file start');
-// Проверяем то, на чем сработало событие
-    let target = event.target || event.srcElement || event.currentTarget;  // Кроссбраузерность?
+
+    let target = event.target || event.srcElement || event.currentTarget;
     let file = target.files[0];
     let xhr = new XMLHttpRequest();
     xhr.open('POST', '/uploads/' + file.name, true);
-// Теперь необходимо послать правильные заголовки
+
     xhr.setRequestHeader('Content-Type', 'application/octate-stream');
-// Когда будет изменяться состояние запроса будем делать:
+
     xhr.onreadystatechange = function () {
         event = null;
         if (xhr.readyState === 4) {
